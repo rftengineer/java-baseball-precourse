c927b7a3f09ccacb6cda090c5d2885f64c91b150
Steve Shim <nightpw1@gmail.com>
2022-04-17 23:35:46 +0900

    feat(Judgement): Ball, Strike 숫자를 반환하는 Map 생성, judge 메서드 의미 변경, size 체크 부분은 validateLength로 변경

--- src/main/java/baseball/model/Judgement.java
+++ src/main/java/baseball/model/Judgement.java
@@ -1,10 +1,43 @@
 package baseball.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Judgement {
 
-    public static boolean judge(String value) {
-        return value.length() == 3;
+    public Map<String, Integer> judge(String inputValue, String answer) {
+        validateLength(inputValue);
+        int ball = countOfBall(inputValue, answer);
+        int strike = countOfStrike(inputValue, answer);
+        Map<String, Integer> result = new HashMap<>();
+        result.putIfAbsent("Ball", ball);
+        result.putIfAbsent("Strike", strike);
+
+        return result;
     }
 
+    private static void validateLength(String value) {
+        if (value.length() != 3) {
+            throw new IllegalArgumentException("입력 숫자의 자릿수는 3자리여야 합니다.");
+        }
+    }
+
+    private int countOfStrike(String inputValue, String answer) {
+        char[] characterOfInputValue = inputValue.toCharArray();
+        char[] characterOfAnswer = answer.toCharArray();
+        int count = 0;
+        for (int index = 0; index < 3; index++) {
+            count += characterOfInputValue[index] == characterOfAnswer[index] ? 1 : 0;
+        }
+        return count;
+    }
+
+    private int countOfBall(String inputValue, String answer) {
+        int count = 0;
+        for (Character c : inputValue.toCharArray()) {
+            count += answer.contains(c.toString()) ? 1 : 0;
+        }
+        return count;
+    }
 
 }
--- src/test/java/baseball/model/JudgementTest.java
+++ src/test/java/baseball/model/JudgementTest.java
@@ -1,16 +1,35 @@
 package baseball.model;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 public class JudgementTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"453,true", "535,true", "23,false", "4423,false", "5,false"})
-    void CsvSource_Test_For_Length3_Judgement(String element, boolean expected) {
-        assertThat(Judgement.judge(element)).isEqualTo(expected);
+    @CsvSource(value = {"4533, 354", "1, 245"})
+    void CsvSource_Test_For_Length3_Judgement(String inputValue, String answer) {
+        Judgement judgement = new Judgement();
+
+        assertThatThrownBy(() -> judgement.judge(inputValue, answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력 숫자의 자릿수는 3자리여야 합니다.");
+
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"354, 153, 2", "123, 456, 0", "132, 213, 3", "156,573, 1"})
+    void CsvSource_Test_For_Ball_Judgement(String inputValue, String answer, int expected) {
+        Judgement judgement = new Judgement();
+        assertThat(judgement.judge(inputValue, answer).get("Ball")).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123, 154, 1", "123, 124, 2", "135, 135, 3", "123, 456, 0"})
+    void CsvSource_Test_For_Strike_Judgement(String inputValue, String answer, int expected) {
+        Judgement judgement = new Judgement();
+        assertThat(judgement.judge(inputValue, answer).get("Strike")).isEqualTo(expected);
     }
 }
