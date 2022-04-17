19103a72a68c776bd400bcbf1da79e74cf9bbe96
Steve Shim <nightpw1@gmail.com>
2022-04-18 01:10:51 +0900

    feat(View): 게임 기능을 위한 뷰 구현 완료

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,12 @@
+package baseball.view;
+
+public class OutputView {
+
+    public static void showResultWithGameScore(String value) {
+        System.out.println(value);
+    }
+
+    public static void clearTheGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
