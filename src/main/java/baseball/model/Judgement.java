64eca4318c3d6f1435bd5a3212d99b060a4e81bc
Steve Shim <nightpw1@gmail.com>
2022-04-18 04:07:47 +0900

    fix(Judgement): 어색한 Method 명 수정, Magic Number 제거, judge 메소드 안의 구체적인 행위를 Method 추출해서 수정

--- src/main/java/baseball/model/Judgement.java
+++ src/main/java/baseball/model/Judgement.java
@@ -6,6 +6,8 @@ import java.util.Map;
 public class Judgement {
     public static final String STRIKE = "Strike";
     public static final String BALL = "Ball";
+    public static final int LIMIT_OF_STRING_LENGTH = 3;
+    public static final int ZERO = 0;
     private final Map<String, Integer> result;
 
     public Judgement() {
@@ -21,19 +23,23 @@ public class Judgement {
     public Map<String, Integer> judge(String inputValue, String answer) {
         validateLength(inputValue);
         initializeResultMap();
-        int strike = countOfStrike(inputValue, answer);
-        int ball = countOfBall(inputValue, answer) - strike;
-        if(ball < 0) {
-            ball = 0;
-        }
-        result.put(BALL, ball);
-        result.put(STRIKE, strike);
+        calculateAndStoreResultMap(inputValue, answer);
 
         return result;
     }
 
+    private void calculateAndStoreResultMap(String inputValue, String answer) {
+        int strike = countOfStrike(inputValue, answer);
+        int ball = countOfBall(inputValue, answer) - strike;
+        if(ball < ZERO) {
+            ball = ZERO;
+        }
+        result.put(BALL, ball);
+        result.put(STRIKE, strike);
+    }
+
     private static void validateLength(String value) {
-        if (value.length() != 3) {
+        if (value.length() != LIMIT_OF_STRING_LENGTH) {
             throw new IllegalArgumentException("입력 숫자의 자릿수는 3자리여야 합니다.");
         }
     }
@@ -42,26 +48,32 @@ public class Judgement {
         char[] characterOfInputValue = inputValue.toCharArray();
         char[] characterOfAnswer = answer.toCharArray();
         int count = 0;
-        for (int index = 0; index < 3; index++) {
-            count += isCharacterStrike(characterOfInputValue, characterOfAnswer, index);
+        for (int index = 0; index < LIMIT_OF_STRING_LENGTH; index++) {
+            count += valueForStrike(characterOfInputValue, characterOfAnswer, index);
         }
         return count;
     }
 
-    private int isCharacterStrike(char[] characterOfInputValue, char[] characterOfAnswer, int index) {
-        return characterOfInputValue[index] == characterOfAnswer[index] ? 1 : 0;
+    private int valueForStrike(char[] characterOfInputValue, char[] characterOfAnswer, int index) {
+        if(characterOfInputValue[index] == characterOfAnswer[index]) {
+            return 1;
+        }
+        return 0;
     }
 
     private int countOfBall(String inputValue, String answer) {
         int count = 0;
         for (Character c : inputValue.toCharArray()) {
-            count += isCharacterBall(answer, c);
+            count += valueForBall(answer, c);
         }
         return count;
     }
 
-    private int isCharacterBall(String answer, Character c) {
-        return answer.contains(c.toString()) ? 1 : 0;
+    private int valueForBall(String answer, Character c) {
+        if (answer.contains(c.toString())) {
+            return 1;
+        }
+        return 0;
     }
 
 }
