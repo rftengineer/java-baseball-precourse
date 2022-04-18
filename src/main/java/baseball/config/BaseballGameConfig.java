09b27858e708109668d46df19780d520da04cc8a
Steve Shim <nightpw1@gmail.com>
2022-04-18 20:34:22 +0900

    feat(GameRule): Game에서 주로 쓰이는 상수조건들, 게임을 컨트롤하는 Config를 추가

---
+++ src/main/java/baseball/config/BaseballGameConfig.java
@@ -0,0 +1,8 @@
+package baseball.config;
+
+public final class BaseballGameConfig {
+    public static final int MIN_VALUE = 1;
+    public static final int MAX_VALUE = 9;
+    public static final int LENGTH_OF_INPUT = 3;
+    public static final int CONDITION_FOR_WIN = 3;
+}
